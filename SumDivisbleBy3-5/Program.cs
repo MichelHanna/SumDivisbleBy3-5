952b097d99d3fc5ee98da077843c683452e7b934
Michel Hanna <Michel.Magdyy@gmail.com>
2019-01-02 04:47:29 +0200

    init project

--- SumDivisbleBy3-5/Program.cs
+++ SumDivisbleBy3-5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SumDivisbleBy3_5
 {
@@ -6,7 +9,38 @@ namespace SumDivisbleBy3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter max number to calculate sum of divisibles of 3 & 5 or exit to quit");
+            try
+            {
+                var choice = "";
+                string maxNumber = Console.ReadLine();
+
+                while (maxNumber != "exit")
+                {
+                    decimal sum = GetDivisbles(Convert.ToDecimal(maxNumber));
+                    Console.WriteLine(sum);
+                    maxNumber = Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+        private static int GetDivisbles(decimal maxNumber)
+        {
+            int Sum = 0;
+            try
+            {
+                for (int counter = 1; counter < maxNumber; counter++)
+                    Sum += counter % 3 == 0 || counter % 5 == 0 ? counter : 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Sum;
+        }
+
     }
 }
