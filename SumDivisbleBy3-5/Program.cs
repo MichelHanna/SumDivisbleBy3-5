f4d45ef49139013703826d303d9a091fe47d14b1
Michel Hanna <Michel.Magdyy@gmail.com>
2019-01-02 04:08:10 +0200

    Add project files.

---
+++ SumDivisbleBy3-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumDivisbleBy3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
